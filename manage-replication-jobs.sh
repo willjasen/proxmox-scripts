84d7130d624a94ca99c6f9dfd8b6eba22c9797b8
willjasen <brandon@willjasen.com>
2024-02-13 19:43:01 -0500

    lots of changes

--- manage-replication-jobs.sh
+++ manage-replication-jobs.sh
@@ -1,30 +1,64 @@
 #!/bin/bash
 
 # --- PARAMETERS ---
-# - action can be either 'run' or 'read'
-#   - run will update the jobs and read will save the current job configuration
+# - first parameter can be either 'read' or 'update'
+#   - update will update the jobs and read will save the current job configuration
+
+
+# --- CONSTANTS ---
+SETTINGS_FILE=replication-job-settings.csv;
+
+
+# --- FUNCTIONS ---
+
+read_jobs () {
+  # Delete the existing settings
+  if test -f $SETTINGS_FILE; then
+    rm $SETTINGS_FILE;
+  fi
+
+  # Create header line in CSV
+  echo "jobID,schedule" | tee -a $SETTINGS_FILE;
+
+  # Save all the current job settings into the settings file
+  jobIDs=$(pvesr list | awk '{print $1}' | grep -Eo '[1-9][0-9]{2,8}-[0-9]{1,9}');
+  for jobID in $jobIDs; do
+    curJobSchedule=$(pvesr read $jobID | jq -r '.schedule');
+    echo "$jobID,$curJobSchedule" | tee -a $SETTINGS_FILE;
+  done;
+}
+
+update_jobs () {
+  # Read in the CSV file
+  while IFS="," read -r jobID schedule
+  do
+    if [ ! -z "$jobID" ]; then
+
+      curJobSchedule=$(pvesr read $jobID | jq -r '.schedule');
+      if [[ "$curJobSchedule" != "$schedule" ]]; then
+        echo "Updating job ID '$jobID' to schedule of '$schedule'";
+        pvesr update $jobID --schedule "$schedule";
+        echo "";
+      else
+        echo "Job ID '$jobID' already has the schedule '$schedule'";
+      fi
+
+    fi
+  done < <(tail -n +2 $SETTINGS_FILE)
+}
+
+# -- END FUNCTIONS ---
 
 # Install jq if not detected
 if [ -z $(which jq) ]; then
   apt install jq -y;
 fi
 
-# Get all job IDs on the current server
-# jobIDs=$(pvesr list | awk '{print $1}' | grep -Eo '[1-9][0-9]{2,8}-[0-9]{1,9}');
-
-# Read in the CSV file
-while IFS="," read -r jobID schedule
-do
-  if [ ! -z "$jobID" ]; then
-
-    curJobSchedule=$(pvesr read $jobID | jq -r '.schedule');
-    if [[ "$curJobSchedule" != "$schedule" ]]; then
-      echo "Updating job ID '$jobID' to schedule of '$schedule'";
-      pvesr update $jobID --schedule "$schedule";
-      echo "";
-    else
-      echo "Job ID '$jobID' already has the schedule '$schedule'";
-    fi
-
-  fi
-done < <(tail -n +2 replication-job-settings.csv)
+# Determine how script should run
+if [[ "$1" == "read" ]]; then
+  read_jobs
+elif [[ "$1" == "update" ]]; then
+  update_jobs
+else
+  echo "Script parameter was not valid";
+fi
