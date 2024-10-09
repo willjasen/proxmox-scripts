4e29f82e3df26f54836768dc3be518547df4e1e5
willjasen <brandon@willjasen.com>
2024-10-09 10:19:04 -0400

    stash changes

--- migrate-ct-with-shared-mountpoint.sh
+++ migrate-ct-with-shared-mountpoint.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
+###
 ### This script will migrate an LXC container that has a shared mount point from one host to another
+###
 
-HOST_TO_MIGRATE_FROM=pve2;
-HOST_TO_MIGRATE_TO=pve1;
+HOST_TO_MIGRATE_FROM=pve1;
+HOST_TO_MIGRATE_TO=pve2;
 LXC_ID=517;
 
 # Comment out the bind mount in the LXC config
-sed -i 's|^mp8: /mnt/pve/cephfs--hdd--lowT/data/syncthing/Media,mp=/mnt/host/Media|# &|' /etc/pve/nodes/$HOST_TO_MIGRATE_FROM/lxc/$LXC_ID.conf
+sed -i 's|^mp8|# &|' /etc/pve/nodes/$HOST_TO_MIGRATE_FROM/lxc/$LXC_ID.conf
 
 # Start the migration process
 pct migrate $LXC_ID $HOST_TO_MIGRATE_TO &
 
 # Wait for the migration to complete
-while pct status $LXC_ID | grep -q 'status: running'; do
+while pct status $LXC_ID | grep -q 'status:'; do
     sleep 5
 done
-
 echo "LXC container $LXC_ID has been successfully migrated to $HOST_TO_MIGRATE_TO"
 
 # (Optional) Re-enable the bind mount on the target host after migration
