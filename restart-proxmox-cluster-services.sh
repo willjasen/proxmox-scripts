9889d88313a4dda76e18eca441f6af5f6157645c
willjasen <brandon@willjasen.com>
2025-02-23 18:29:39 -0500

    add duration in output

--- restart-proxmox-cluster-services.sh
+++ restart-proxmox-cluster-services.sh
@@ -3,8 +3,12 @@ RESET="\e[0m"
 
 restart_service() {
     local service=$1
-    echo -e "${GREEN}Restarting ${service}...${RESET}"
+    echo -ne "${GREEN}Restarting ${service}...${RESET}"
+    local start_time=$(date +%s)
     systemctl restart ${service}
+    local end_time=$(date +%s)
+    local duration=$((end_time - start_time))
+    echo -e "${GREEN} done in ${duration} seconds.${RESET}"
 }
 
 restart_service corosync
