6886bdcad03b84e84101d7b0f7f3f1d7931a8f2b
willjasen <brandon@willjasen.com>
2025-04-02 23:37:14 -0400

    install iostat if needed

--- show-iostat.sh
+++ show-iostat.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+command -v iostat >/dev/null || { echo "iostat not found. Please install the sysstat package." >&2; exit 1; }
+
 iostat -d -m | (
 	head -3;
 	tail -n +4 | awk '$3 != "0.00"' | sort -k 3 -nr
