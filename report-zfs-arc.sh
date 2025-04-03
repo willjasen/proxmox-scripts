ea24331a6e5df24d057b0394574554644d7ce465
willjasen <brandon@willjasen.com>
2025-04-03 08:53:30 -0400

    try a fix

--- report-zfs-arc.sh
+++ report-zfs-arc.sh
@@ -6,9 +6,9 @@ arc_data=$(arc_summary)
 # Display arc_summary output in a better tabular format
 # echo "$arc_data" | sed '/^$/d' | column -t -s ':' | sed 's/^/ /'
 
-# Extract values (assuming lines like "ARC Size: <number>" and "ARC Used: <number>")
-arc_size=$(echo "$arc_data" | grep -i "ARC Size" | head -n1 | cut -d':' -f2 | tr -d ' ')
-arc_used=$(echo "$arc_data" | grep -i "ARC Used" | head -n1 | cut -d':' -f2 | tr -d ' ')
+# Extract values and filter out any non-numeric characters (except dot)
+arc_size=$(echo "$arc_data" | grep -i "ARC Size" | head -n1 | cut -d':' -f2 | sed 's/[^0-9.]//g')
+arc_used=$(echo "$arc_data" | grep -i "ARC Used" | head -n1 | cut -d':' -f2 | sed 's/[^0-9.]//g')
 
 # Calculate percentage used (using bc for floating point division)
 percentage=$(echo "scale=2; $arc_used * 100 / $arc_size" | bc)
