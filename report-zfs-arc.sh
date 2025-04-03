9c6bc2148e3d969262f3eb7a2c31b41203a58e17
willjasen <brandon@willjasen.com>
2025-04-03 08:52:50 -0400

    try a stat

--- report-zfs-arc.sh
+++ report-zfs-arc.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+# Capture arc_summary output once
+arc_data=$(arc_summary)
+
 # Display arc_summary output in a better tabular format
-arc_summary | sed '/^$/d' | column -t -s ':' | sed 's/^/ /'
+# echo "$arc_data" | sed '/^$/d' | column -t -s ':' | sed 's/^/ /'
+
+# Extract values (assuming lines like "ARC Size: <number>" and "ARC Used: <number>")
+arc_size=$(echo "$arc_data" | grep -i "ARC Size" | head -n1 | cut -d':' -f2 | tr -d ' ')
+arc_used=$(echo "$arc_data" | grep -i "ARC Used" | head -n1 | cut -d':' -f2 | tr -d ' ')
+
+# Calculate percentage used (using bc for floating point division)
+percentage=$(echo "scale=2; $arc_used * 100 / $arc_size" | bc)
+
+echo ""
+echo "ZFS ARC used: ${percentage}%"
