7ccc29aeeac78f750039e532540d3c83afb621de
willjasen <brandon@willjasen.com>
2025-08-03 18:52:25 -0400

    only calculate online VMs

--- report-vm-memory.sh
+++ report-vm-memory.sh
@@ -5,10 +5,13 @@ RESET="\e[0m"
 
 total_memory=0
 for vmid in $(qm list | awk 'NR>1 {print $1}'); do
-    name=$(qm config "$vmid" | grep '^name:' | awk '{print $2}')  # extract VM name
-    mem=$(qm config "$vmid" | grep '^memory:' | awk '{print $2}')
-    echo "${name}: ${mem} MB"  # new echo for each VM's RAM with name
-    total_memory=$((total_memory + mem))
+    status=$(qm status "$vmid" | grep '^status:' | awk '{print $2}')  # check VM status
+    if [ "$status" == "running" ]; then
+        name=$(qm config "$vmid" | grep '^name:' | awk '{print $2}')  # extract VM name
+        mem=$(qm config "$vmid" | grep '^memory:' | awk '{print $2}')
+        echo "${name}: ${mem} MB"  # new echo for each VM's RAM with name
+        total_memory=$((total_memory + mem))
+    fi
 done
 # Convert total memory from MB to GB with 2 decimal places
 total_memory_gb=$(echo "scale=2; ${total_memory}/1024" | bc)
