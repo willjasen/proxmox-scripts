ad0f4b0177aa670503462ffc7549f7147b0cb8b4
willjasen <brandon@willjasen.com>
2025-08-03 18:12:38 -0400

    output the last part in green

--- report-vm-memory.sh
+++ report-vm-memory.sh
@@ -7,9 +7,9 @@ for vmid in $(qm list | awk 'NR>1 {print $1}'); do
 done
 # Convert total memory from MB to GB with 2 decimal places
 total_memory_gb=$(echo "scale=2; ${total_memory}/1024" | bc)
-echo "Total Configured RAM for all VMs: ${total_memory_gb} GB"
+echo -e "\e[32mTotal Configured RAM for all VMs: ${total_memory_gb} GB\e[0m"
 
 # Retrieve and display the physical host's total RAM
 host_memory_kb=$(grep MemTotal /proc/meminfo | awk '{print $2}')
 host_memory_gb=$(echo "scale=2; ${host_memory_kb}/1024/1024" | bc)
-echo "Total Physical Host RAM: ${host_memory_gb} GB"
+echo -e "\e[32mTotal Physical Host RAM: ${host_memory_gb} GB\e[0m"
