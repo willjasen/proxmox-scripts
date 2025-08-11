b0b08a2f1f140c90c8105b30b402c7f805e83c05
willjasen <brandon@willjasen.com>
2025-08-10 20:00:07 -0400

    show in groups

--- report-vm-start-priorities.sh
+++ report-vm-start-priorities.sh
@@ -2,38 +2,39 @@
 # Report Proxmox VM and CT startup order
 
 printf "%-4s %-6s %-6s %-12s %-12s %s\n" "TYPE" "ID" "ONBOOT" "START ORDER" "START DELAY" "NAME"
-echo "TYPE|ID|ONBOOT|START ORDER|START DELAY|NAME"
+
 
 # Function to get VM info
 get_vm_info() {
-	for VMID in $(qm list | awk 'NR>1 {print $1}'); do
-		NAME=$(qm config $VMID | awk -F ': ' '/^name: /{print $2}')
-		ONBOOT=$(qm config $VMID | awk -F ': ' '/^onboot: /{print $2}')
-		ORDER=$(qm config $VMID | awk -F ': ' '/^order: /{print $2}')
-		STARTDELAY=$(qm config $VMID | awk -F ': ' '/^startup: /{print $2}' | awk -F "," '{for(i=1;i<=NF;i++){if($i ~ /^up=/){print substr($i,4)}}}')
-		[ -z "$ONBOOT" ] && ONBOOT="no"
-		[ -z "$ORDER" ] && ORDER=""
-		[ -z "$STARTDELAY" ] && STARTDELAY=""
-		echo "VM|$VMID|$ONBOOT|${ORDER:-}|${STARTDELAY:-}|$NAME"
-	done
+       for VMID in $(qm list | awk 'NR>1 {print $1}'); do
+	       NAME=$(qm config $VMID | awk -F ': ' '/^name: /{print $2}')
+	       ORDER=$(qm config $VMID | awk -F ': ' '/^order: /{print $2}')
+	       [ -z "$ORDER" ] && ORDER="none"
+	       echo -e "$ORDER\t$NAME"
+       done
 }
 
 # Function to get CT info
 get_ct_info() {
-	for CTID in $(pct list | awk 'NR>1 {print $1}'); do
-		NAME=$(pct config $CTID | awk -F ': ' '/^hostname: /{print $2}')
-		ONBOOT=$(pct config $CTID | awk -F ': ' '/^onboot: /{print $2}')
-		ORDER=$(pct config $CTID | awk -F ': ' '/^order: /{print $2}')
-		STARTDELAY=$(pct config $CTID | awk -F ': ' '/^startup: /{print $2}' | awk -F "," '{for(i=1;i<=NF;i++){if($i ~ /^up=/){print substr($i,4)}}}')
-		[ -z "$ONBOOT" ] && ONBOOT="no"
-		[ -z "$ORDER" ] && ORDER=""
-		[ -z "$STARTDELAY" ] && STARTDELAY=""
-		echo "CT|$CTID|$ONBOOT|${ORDER:-}|${STARTDELAY:-}|$NAME"
-	done
+       for CTID in $(pct list | awk 'NR>1 {print $1}'); do
+	       NAME=$(pct config $CTID | awk -F ': ' '/^hostname: /{print $2}')
+	       ORDER=$(pct config $CTID | awk -F ': ' '/^order: /{print $2}')
+	       [ -z "$ORDER" ] && ORDER="none"
+	       echo -e "$ORDER\t$NAME"
+       done
 }
 
 # Collect and sort all info by START ORDER (order field, then ID as fallback)
 {
-	get_vm_info
-	get_ct_info
-} | sort -t '|' -k4,4n -k2,2n | column -t -s '|'
+	   get_vm_info
+	   get_ct_info
+} | sort -k1,1n | awk -F '\t' '
+BEGIN { last_order = "" }
+{
+	if ($1 != last_order) {
+		if (last_order != "") print "";
+		print "Start Order: "$1;
+		last_order = $1;
+	}
+	print "  "$2;
+}'
