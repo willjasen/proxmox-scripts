f0a1ae282213694473e3bda02182464dda6d33d1
willjasen <brandon@willjasen.com>
2025-08-28 21:19:10 -0400

    mount SSHFS

--- export-vms.sh
+++ export-vms.sh
@@ -4,11 +4,25 @@
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-# array of VM IDs you want to copy
+# array of VM IDs to copy
 VMIDS=(702 703 703 705 706 707 709 710)
 
+# Remote server to copy to
+REMOTE_HOST="pve2"
+REMOTE_DIR="/mnt/zfs/from-pve4"
+LOCAL_MOUNT="/mnt/to-$REMOTE_HOST"
+
+# Install dependencies
+apt-get update && apt-get install -y sshfs 
+
+# Setup SSHFS mount
+sshfs root@$REMOTE_HOST:$REMOTE_DIR $LOCAL_MOUNT
+df -Th $LOCAL_MOUNT
+
+# Export the VMs
 for id in "${VMIDS[@]}"; do
   echo -e "${GREEN}Exporting VM $id ...${NC}"
+  # Export a VM
   qemu-img convert -p -O qcow2 -c -o compression_type=zstd \
     /dev/lvm-417/vm-${id}-disk-0 \
     /mnt/to-pve2/vm-${id}-disk-0.qcow2
